f5750cf69aaa3ce4be4a2a34e8630cb229ab626c
Peter Galantha <19354642+peterg79@users.noreply.github.com>
2025-05-21 18:39:18 -0700

    automatically try hosts for upload (#9)

--- upload.sh
+++ upload.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-HOSTNAME=172.27.201.232
+HOSTNAMES=(172.27.201.232 192.168.4.1)
 
-for file in $(ls -1 data); do
-    echo "Uploading $file"
-    curl -F "data=@data/$file" http://$HOSTNAME/upload
+
+for host in ${HOSTNAMES[@]}; do
+    echo "Trying $host ..."
+    if ping $host -q -t 1 -o &>/dev/null; then 
+        for file in $(ls -1 data); do
+            echo "Uploading $file"
+            curl -F "data=@data/$file" http://${host}/upload
+        done
+    else
+        echo "Nope."
+    fi
 done
